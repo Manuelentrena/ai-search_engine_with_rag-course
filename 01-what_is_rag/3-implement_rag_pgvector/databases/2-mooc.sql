fa0ad8239546ad5ea6e341a506d706a1d22c60c2
EntrenaDev <mentrena@aicor.com>
2026-01-01 18:27:29 +0100

    feat: update embedding vector size to 1536 in courses table

--- 01-what_is_rag/3-implement_rag_pgvector/databases/2-mooc.sql
+++ 01-what_is_rag/3-implement_rag_pgvector/databases/2-mooc.sql
@@ -3,5 +3,5 @@ CREATE SCHEMA IF NOT EXISTS mooc;
 CREATE TABLE IF NOT EXISTS mooc.courses (
     id CHAR(4) PRIMARY KEY NOT NULL,
     name VARCHAR(255) NOT NULL,
-    embedding vector(768)
+    embedding vector(1536)
 );
