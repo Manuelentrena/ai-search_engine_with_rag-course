e38d6d80bb1b6df431b8d474a381578340b40dcb
EntrenaDev <manuel.entrena@gmail.com>
2026-01-19 12:49:29 +0100

    fix: update embedding function in SQL vectorizer for OpenAI compatibility

--- 04-what_db_to_choose/1-pgai/1-simple_example/databases/2-add_vectorizer_to_courses.sql
+++ 04-what_db_to_choose/1-pgai/1-simple_example/databases/2-add_vectorizer_to_courses.sql
@@ -1,6 +1,6 @@
 SELECT ai.create_vectorizer(
 	'mooc.courses'::regclass,
-	embedding => ai.embedding_ollama('nomic-embed-text', 768),
+	embedding => ai.embedding_openai('text-embedding-3-small', 768, api_key_name=>'OPENAI_API_KEY'),
 	chunking => ai.chunking_character_text_splitter('summary', 128, 10),
 	formatting => ai.formatting_python_template('Name: $name | Summary: $chunk | Categories: $categories')
 );
